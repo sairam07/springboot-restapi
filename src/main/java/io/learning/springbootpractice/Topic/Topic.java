2edd204ffb078ee06dc270c94ff1f8ae36f7a235
sairam2093 <sai.adusumilli@gmail.com>
2017-02-11 21:29:37 -0600

    Adding topic class

---
+++ src/main/java/io/learning/springbootpractice/Topic/Topic.java
@@ -0,0 +1,35 @@
+package io.learning.springbootpractice.Topic;
+
+public class Topic {
+	private String tpoicID;
+	private String topicName;
+	private String topicDescription;
+	
+	
+	public Topic(String tpoicID, String topicName, String topicDescription) {
+		super();
+		this.tpoicID = tpoicID;
+		this.topicName = topicName;
+		this.topicDescription = topicDescription;
+	}
+	public String getTpoicID() {
+		return tpoicID;
+	}
+	public void setTpoicID(String tpoicID) {
+		this.tpoicID = tpoicID;
+	}
+	public String getTopicName() {
+		return topicName;
+	}
+	public void setTopicName(String topicName) {
+		this.topicName = topicName;
+	}
+	public String getTopicDescription() {
+		return topicDescription;
+	}
+	public void setTopicDescription(String topicDescription) {
+		this.topicDescription = topicDescription;
+	}
+	
+
+}
